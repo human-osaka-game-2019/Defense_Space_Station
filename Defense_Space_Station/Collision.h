ba543c9f0c21b52b7f3c8215475d7bf17fa6f8c3
UENO <36749546+k1rrn@users.noreply.github.com>
2019-08-22 16:59:20 +0900

    矩形と矩形の当たり判定を追加。 通り抜けブラックの当たり反省を追加。

--- Defense_Space_Station/Collision.h
+++ Defense_Space_Station/Collision.h
@@ -2,14 +2,51 @@
 #define Collision_H_
 
 
-class Collision
+class JudgeCollision
 {
 public:
-	Collision();
-	~Collision();
+
+	bool RectCollion(
+		float FirstRectX,float FirstRectY,
+		int FirstRectWhith, int FirstRectHight,
+		float SecondRectX, float SecondRectY,
+		int SecondRectWhith, int ScondRectHight);
+
+	bool AirBloackCollion(
+		float FirstRectX, float FirstRectY,
+		int FirstRectWhith, int FirstRectHight,
+		float SecondRectX, float SecondRectY,
+		int SecondRectWhith, int ScondRectHight,
+		int PastPosY);
 
 private:
 
 };
 
 #endif // !Collision_H_
+
+/*
+
+bool CollisionAirBlock(
+	FLOAT PlayerPosX, FLOAT PlayerPosY,
+	INT PlayerWhith, INT PlayerHight,
+	FLOAT EnemyPosX, FLOAT EnemyPosY,
+	INT EnemyWhith, INT fEnemy_Hight)
+{
+	if (PlayerPosX + PlayerWhith >= EnemyPosX
+		&&
+		PlayerPosX <= EnemyPosX + EnemyWhith)
+
+		if (PlayerPosY + PlayerHight >= EnemyPosY
+			&&
+			PlayerPosY <= EnemyPosY + fEnemy_Hight)
+		{
+			if (CurrentSpotY > PlayerPosY)
+			{
+				return false;
+			}
+			else return true;
+		}
+	return false;
+}
+*/
