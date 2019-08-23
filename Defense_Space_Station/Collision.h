011eac57267d4d1341a6922df849700c2f2db3bf
UENO <36749546+k1rrn@users.noreply.github.com>
2019-08-23 15:01:25 +0900

    [#14]不要なコメントの削除。

--- Defense_Space_Station/Collision.h
+++ Defense_Space_Station/Collision.h
@@ -24,29 +24,3 @@ private:
 };
 
 #endif // !Collision_H_
-
-/*
-
-bool CollisionAirBlock(
-	FLOAT PlayerPosX, FLOAT PlayerPosY,
-	INT PlayerWhith, INT PlayerHight,
-	FLOAT EnemyPosX, FLOAT EnemyPosY,
-	INT EnemyWhith, INT fEnemy_Hight)
-{
-	if (PlayerPosX + PlayerWhith >= EnemyPosX
-		&&
-		PlayerPosX <= EnemyPosX + EnemyWhith)
-
-		if (PlayerPosY + PlayerHight >= EnemyPosY
-			&&
-			PlayerPosY <= EnemyPosY + fEnemy_Hight)
-		{
-			if (CurrentSpotY > PlayerPosY)
-			{
-				return false;
-			}
-			else return true;
-		}
-	return false;
-}
-*/
