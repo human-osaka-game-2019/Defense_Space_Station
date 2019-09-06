07f19e459720d3a4475c117b902974a6a0b3e7d4
OhuraKazuma <k.ohura0303@gmail.com>
2019-09-06 18:13:28 +0900

    :sparkles: [#13] タイトルシーンの作成。

--- Defense_Space_Station/Header/Title.h
+++ Defense_Space_Station/Header/Title.h
@@ -1,6 +1,11 @@
 ﻿#ifndef TITLE_H_
 #define TITLE_H_
 
+#include "../Header/Main.h"
+#include "../Header/Jump.h"
+#include "../Header/Vector.h"
+#include "../Header/Size.h"
+
 class TITLE {
 public:
 	TITLE();
@@ -17,10 +22,27 @@ private:
 
 	STEP step = LoadStep;
 
+	const Vec BackGroundPos = { 0, 0 };
+	const Size BackGroundSize = { DISPLAY_WIDTH, DISPLAY_HEIGHT };
+	Vec LogoCenterPos = { 924.75, 540 };
+	Size LogoSize = { DISPLAY_WIDTH, DISPLAY_HEIGHT };
+	Vec PushEnterPos = { 0, 0 };
+	const Size PushEnterSize = { DISPLAY_WIDTH , DISPLAY_HEIGHT };
+
+	float jump_speed = 0;
+
+	bool will_big = true;
+	float logo_zoom = 1.0f;
+	float ZoomSpeed = 0.005f;
+	const float MaxZoom = 1.10f;
+	const float MinZoom = 0.9f;
+
 	void Load();
 	void Control();
 	void Draw();
 	void Release();
+
+	void ZoomLogo();
 
 };
 
